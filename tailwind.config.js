dda8505c3073024a27064575983f2c4451bcae36
Madipta <madipta@gmail.com>
2020-12-26 16:22:50 +0700

    initial comit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  purge: ['./apps/**/*.html', './apps/**/*.ts'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
