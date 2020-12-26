d88efc1d9a6c5d8ae549a415a4b36ea18b292541
Madipta <madipta@gmail.com>
2020-12-26 22:29:12 +0700

    initial commit

--- libs/ui/angular/src/lib/contact-list/contact-list.component.ts
+++ libs/ui/angular/src/lib/contact-list/contact-list.component.ts
@@ -11,13 +11,13 @@ import { Component } from '@angular/core';
           <img [alt]="contact.name" [src]="contact.photoUrl" class="h-12 w-12 rounded-full mx-auto">
         </div>
         <div class="flex flex-col w-4/6 text-sm mx-3">
-          <h3 class="text-gray-800 font-semibold leading-tight">{{contact.name}}</h3>
+          <h3 class="text-gray-600 font-extrabold leading-tight">{{contact.name}}</h3>
           <p class="text-gray-500 whitespace-nowrap overflow-ellipsis overflow-hidden">{{contact.lastMessage}}</p>
         </div>
-        <div class="flex flex-col w-1/6 flex-shrink text-center text-xs text-gray-600">
+        <div class="flex flex-col w-1/6 flex-shrink text-center text-xs text-gray-500">
           <div>{{contact.lastActive}}</div>
           <div class="flex justify-center mt-1">
-            <span class="bg-green-500 text-white leading-relaxed px-1 rounded-full">
+            <span class="bg-green-400 text-white leading-6 px-2 rounded-full">
               {{contact.newMessageCount}}
             </span>
           </div>
@@ -45,35 +45,35 @@ export class ContactListComponent {
     {
       name: 'Salvatore Gravano',
       lastActive: 'yesterday',
-      newMessageCount: 5,
+      newMessageCount: 123,
       photoUrl: 'https://randomuser.me/api/portraits/men/79.jpg',
       lastMessage: 'Don\'t ever take sides with anyone against the family.'
     },
     {
       name: 'Giuseppe Masseria',
       lastActive: 'yesterday',
-      newMessageCount: 5,
+      newMessageCount: 1231,
       photoUrl: 'https://randomuser.me/api/portraits/men/82.jpg',
       lastMessage: 'Blood makes you related. Loyalty makes you family.',
     },
     {
       name: 'Anthony Anastasio',
       lastActive: 'yesterday',
-      newMessageCount: 5,
+      newMessageCount: 8,
       photoUrl: 'https://randomuser.me/api/portraits/men/7.jpg',
       lastMessage: 'Knowledge will give you power but character will give you respect.',
     },
     {
       name: 'Vincent Gigante',
       lastActive: 'yesterday',
-      newMessageCount: 5,
+      newMessageCount: 65,
       photoUrl: 'https://randomuser.me/api/portraits/men/40.jpg',
       lastMessage: 'I don\'t trust words, I even question actions but I never doubt patterns',
     },
     {
       name: 'Alessandro Vollero',
       lastActive: 'yesterday',
-      newMessageCount: 5,
+      newMessageCount: 907,
       photoUrl: 'https://randomuser.me/api/portraits/men/29.jpg',
       lastMessage: 'No one gives it to you. You have to take it.',
     },
@@ -81,7 +81,7 @@ export class ContactListComponent {
       name: 'Charlie Luciano',
       position: 'Lucky',
       lastActive: 'yesterday',
-      newMessageCount: 5,
+      newMessageCount: 4,
       photoUrl: 'https://randomuser.me/api/portraits/men/71.jpg',
       lastMessage: 'Keep your friends close and your enemy closer..',
     },
