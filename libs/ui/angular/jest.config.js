2581ed0607bd31eb7d23225b43f678b97c110ea5
Madipta <madipta@gmail.com>
2021-11-23 07:57:09 +0700

    nx update

--- libs/ui/angular/jest.config.js
+++ libs/ui/angular/jest.config.js
@@ -11,7 +11,10 @@ module.exports = {
   },
   coverageDirectory: '../../../coverage/libs/ui/angular',
 
-  transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' },
+  transform: {
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
